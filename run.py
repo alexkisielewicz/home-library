ea3363dcd16acb11f360d1b062d41457bde9a6cb
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-11-01 14:44:39 +0000

    Create function to show app logo and menu

--- run.py
+++ run.py
@@ -1,4 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Hello, World!")
+def welcome():
+    from header import logo, show_menu
+    logo()
+    show_menu()
+
+welcome()
+
+
+
+
+
+
+# TODO
+# * User Interact with the terminal
+# * Validate User input
+# * Exception handling
+# * Termnial colours
+# * Clearing of the screen
+# * Split code into multiple .py files
+# * google_sheets_api.py
+# * write type hinting
+
+# api (--> this is a python library)
+#     __init__.py
+#     google_sheets_api.py
+#     finhub_api.py
+# utils
+#     __init__.py
+#     printing_utils.py
+# constants.py
+# run.py
+#     from api.google_sheets_api import XYZ  # example
