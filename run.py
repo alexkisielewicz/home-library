895951b38084832ebc2050fff1fdff0ef3bae72b
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-11-01 16:03:49 +0000

    Connect Google Sheets API and add modules to requirements.txt

--- run.py
+++ run.py
@@ -1,20 +1,35 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('home-library')
+
+LIBRARY = SHEET.worksheet('library')
+
+
 def welcome():
     from header import logo, show_menu
     logo()
     show_menu()
 
-welcome()
+print(LIBRARY.acell("A1").value)
 
-
-
-
+# welcome()
 
 
 # TODO
 # * User Interact with the terminal
 # * Validate User input
 # * Exception handling
-# * Termnial colours
+# * Terminal colours
 # * Clearing of the screen
 # * Split code into multiple .py files
 # * google_sheets_api.py
@@ -23,7 +38,7 @@ welcome()
 # api (--> this is a python library)
 #     __init__.py
 #     google_sheets_api.py
-#     finhub_api.py
+#     finnhub_api.py
 # utils
 #     __init__.py
 #     printing_utils.py
